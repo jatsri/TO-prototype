5470417fd93a50e54a6fef31078d946fd3fc7ffb
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-05-12 14:16:39 +0200

    Create query.js

--- src/db/query.js
+++ src/db/query.js
@@ -1,5 +1,15 @@
-const getConnection = () => {};
+const { Pool, defaults } = require('pg');
 
-module.exorts = {
-    getConnection
+defaults.parseInt8 = true;
+const pool = new Pool();
+
+const query = (text, params) => {
+    return pool.query(text, params);
+};
+
+const getConnection = () => pool.connect();
+
+module.exports = {
+    query,
+    getConnection,
 };
