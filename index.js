9a4923ec5245afc5d5430e57796b46a3f00821b8
Sri <srivaradhan.naguleswaran@holidaycheck.com>
2020-05-12 11:28:26 +0200

    Create health endpoint

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const handleHealth = require('./src/handlers/handleHealth');
+
 
 const port = 3000;
 const app = express();
@@ -6,3 +10,5 @@ const app = express();
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
 });
+
+app.get('/health', asyncHandler(handleHealth));
